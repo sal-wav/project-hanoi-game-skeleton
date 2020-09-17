ef3c144aa66785ae779e46647644f63ced7ef2cc
salina kuo <salina.kuo97@gmail.com>
2020-09-17 19:04:08 -0400

    writing tests for myMap

---
+++ tdd_project/problems/my-map.js
@@ -0,0 +1,9 @@
+function myMap(array, cb) {
+    // const newArr = [];
+    // array.forEach(el => newArr.push(cb(el)));
+    // return newArr;
+}
+
+
+
+module.exports = myMap;
--- tdd_project/test/my-map-spec.js
+++ tdd_project/test/my-map-spec.js
@@ -1,1 +1,18 @@
- 
+ const chai = require('chai');
+ const expect = chai.expect;
+ const spies = require('chai-spies');
+ chai.use(spies);
+
+ const myMap = require('../problems/my-map.js')
+
+ describe('myMap', function() {
+     let testArray;
+     beforeEach(function() {
+        testArray = [1, 2, 3];
+     })
+     it ('should not mutate the original array', function() {
+         let expected = [1, 2, 3];
+         myMap(testArray, (num) => num*2);
+         expect(testArray).to.eql(expected);
+     })
+ })
