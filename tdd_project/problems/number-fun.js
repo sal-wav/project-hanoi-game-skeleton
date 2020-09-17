d4c0c45cc705abf45bf846e6236ed51f4b03925b
maxwelljmckee <maxwelljmckee@gmail.com>
2020-09-17 16:45:50 -0600

    fun with nums, complete!

---
+++ tdd_project/problems/number-fun.js
@@ -0,0 +1,14 @@
+
+function returnsThree() {
+  return 3;
+}
+
+function reciprocal(num) {
+  return  1 / num;
+}
+
+
+module.exports = {
+  returnsThree,
+  reciprocal
+}
--- tdd_project/test/number-fun-spec.js
+++ tdd_project/test/number-fun-spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { returnsThree, reciprocal } = require('../problems/number-fun');
 
 describe('returnsThree', function() {
     it ('should return the number 3', function() {
@@ -9,10 +10,22 @@ describe('returnsThree', function() {
 })
 
 describe('reciprocal(num)', function() {
-    it ('should intake a number and return the reciprocal', function() {
-        assert.equal(reciprocal(2), 0.5);
-        assert.equal(reciprocal(4), 0.25);
-        assert.equal(reciprocal(5), 0.2);
-        
+    context('if the input is a number not equal to 0', function() {
+        it ('should intake a number and return the reciprocal', function() {
+            assert.equal(reciprocal(2), 0.5);
+            assert.equal(reciprocal(4), 0.25);
+            assert.equal(reciprocal(5), 0.2);
+        })
+    })
+    context('if the input is not a number', function() {
+        it('should return NaN', function() {
+            assert.equal(reciprocal('string'), NaN);
+            assert.equal(reciprocal([1, 2, 3]), NaN)
+        })
+    })
+    context('if input is 0', function() {
+        it('should return Infinity', function() {
+            assert.equal(reciprocal(0), Infinity)
+        })
     })
 })
